b452a96c2aa297990b6004ec4e4e43797a832ed4
UNtergas <mycomputerer135@gmail.com>
2023-04-07 12:19:12 +0000

    Co-authored-by: MinPNG <MinPNG@users.noreply.github.com> Co-authored-by: NGO Minh Phong <MinPNG@users.noreply.github.com>

--- Session/Room/Room.c
+++ Session/Room/Room.c
@@ -16,42 +16,44 @@
 #include <strings.h>
 #include <ifaddrs.h>
 #include <fcntl.h>
+#include "../../Network/IPC_Interface/Message/msg.h"
 
-int isFull(Room room) {
-    if (room.maxPlayer == room.currentNumber) return 1;
+int isFull(Room room)
+{
+    if (room.maxPlayer == room.currentNumber)
+        return 1;
     return 0;
 }
 
-
-
-Room createRoom(char* roomName, int maxPlayer, Player host) {
+Room createRoom(char *roomName, int maxPlayer, Player host)
+{
     Room newRoom;
-    
+
     // Initialize room name
     unsigned long nameLength = strlen(roomName) + 1;
     newRoom.name = malloc(nameLength);
     strncpy(newRoom.name, roomName, nameLength);
     newRoom.name[nameLength] = '\0';
-    
+
     // Initialize player number
     newRoom.maxPlayer = maxPlayer;
-    
+
     // Initialize players of room
-    newRoom.players = malloc(maxPlayer*sizeof(Player));
-    
+    newRoom.players = malloc(maxPlayer * sizeof(Player));
+
     addPlayer(&newRoom, &host);
-    
+
     newRoom.currentNumber = 1;
-    
+
     return newRoom;
 }
-
 
 // The function @destroyRoom is used to end the current room (current session)
 // @parameters: {
 //  @room: The room we want to end
 // }
-int destroyRoom(Room *room) {
+int destroyRoom(Room *room)
+{
     // Step 1: free the @room->name pointer
     // Step 2: free the @room->players pointer
     // Step 3: free the @room pointer
@@ -59,15 +61,14 @@ int destroyRoom(Room *room) {
     // +++ YOUR CODE HERE +++ //
 }
 
-
-
 // The function @discoverRoom is used to discover if there are any active room in current LAN
 // @parameters: {
 //  @hostIPs: used to write the ip address of discovered room's host
 //  @roomNames: used to write the room name of discovered room
 // }
 // @return: 0 if successfully discorver the rooms or -1 if not
-int discorverRoom(char*** hostIPs, char*** roomNames) {
+int discorverRoom(char ***hostIPs, char ***roomNames)
+{
     // Step 1: send to all IP in LAN the following package:
     //      Package's content: ?DiscoverRoom
     // Step 2: wait for the response
@@ -76,17 +77,16 @@ int discorverRoom(char*** hostIPs, char*** roomNames) {
     // +++ YOUR CODE HERE +++ //
 }
 
-
-
 // The function @joinRoom is used to request joining into a room
 // @parameters: {
-//  @hostIPaddr: the IP address of room's host 
+//  @hostIPaddr: the IP address of room's host
 // }
 // @return: 0 if successfully join of -1 if not
-int joinRoom(char *hostIPaddr, char *roomName) {
+int joinRoom(char *hostIPaddr, char *roomName)
+{
     // Step 1: connect with host via @hostIPaddr (using TCP socket)
     // Step 2: send the joining request message
-    //      The message's content: ?Join{@roomName} 
+    //      The message's content: ?Join{@roomName}
     //      replace the {@roomName} with @roomName
     // Step 3: wait for response
     // Step 4: decode response message
@@ -95,15 +95,14 @@ int joinRoom(char *hostIPaddr, char *roomName) {
     // +++ YOUR CODE HERE +++ //
 }
 
-
-
-// The function @connectToRoomNetwork is used to connect to the rest of room (except host) 
+// The function @connectToRoomNetwork is used to connect to the rest of room (except host)
 // The purpose is create a P2P network in room
 // @parameters: {
 //  @room: the room we want to connect to create a P2P network
 //  @clientSocketFds: the list of used to write the new sockets we will create into
 // }
-int connectToRoomNetwork(Room room, int **clientSocketFds) {
+int connectToRoomNetwork(Room room, int **clientSocketFds)
+{
     // Step 1: verify if @clientSocketFds is NULL, if yes => return -1
     // Step 2: verify if *clientSocketFds is NULL, if yes => allocate it as an array of @room.maxPlayer element
     // Step 3: check if the current number of players of rooms (@room.currentNumber) is 2 or not?
@@ -115,30 +114,32 @@ int connectToRoomNetwork(Room room, int **clientSocketFds) {
     // +++ YOUR CODE HERE +++ //
 }
 
-
-
 // The function @sendRoomIn4 is used to send the current room's information to the new player who just join the room
 // @parameters: {
 //  @newPlayerSocketFd: the socket fd of new player
 //  @room: the information of current room
 // }
 // @return: 0 if successfully send the room's in4 or -1 if not
-int sendRoomIn4(int newPlayerSocketFd, Room room) {
+int sendRoomIn4(int newPlayerSocketFd, Room room)
+{
     // Step 1: encoded room's in4 by calling the function @roomToStr from MSGCode module
     // Step 2: send the new encoding message to new player via @newPlayerSocketFd
 
     // +++ YOUR CODE HERE +++ //
 }
 
-
-
-void addPlayer(Room *room, Player *newPlayer) {
-    if (isFull(*room)) return;
-    if (room->players == NULL) {
-        room->players = malloc((room->maxPlayer)*sizeof(Player));
+void addPlayer(Room *room, Player *newPlayer)
+{
+    if (isFull(*room))
+        return;
+    if (room->players == NULL)
+    {
+        room->players = malloc((room->maxPlayer) * sizeof(Player));
     }
-    for (int i=0; i<room->maxPlayer; ++i) {
-        if (isNA(room->players[i])) {
+    for (int i = 0; i < room->maxPlayer; ++i)
+    {
+        if (isNA(room->players[i]))
+        {
             copyPlayer(&room->players[i], newPlayer);
             room->currentNumber += 1;
             return;
@@ -146,15 +147,80 @@ void addPlayer(Room *room, Player *newPlayer) {
     }
 }
 
-
-
 // The function @sendGameStateToNewPlayer is used to send the current game state to the new player of the room
 // parameters: {
 //  @encodedGameState: the game state encoded under the type of string
-//  @newPlayerSocketFd: the socket fd of new player 
+//  @newPlayerSocketFd: the socket fd of new player
 // }
 // @return: 0 if successfully send or -1 if not
-int sendGameStateToNewPlayer(char *encodedGameState, int newPlayerSocketFd) {
+int sendGameStateToNewPlayer(char *filePath, int newPlayerSocketFd)
+{
     // +++ YOUR CODE HERE +++ //
+    FILE *ptr;
+    fopen(filePath, "r");
+    if (ptr == NULL)
+    {
+        printf("file ");
+        return -1;
+    }
+    char buffer[1024];
+    while (!feof(ptr))
+    {
+        size_t bytesRead = fread(buffer, sizeof(char), sizeof(buffer), ptr);
+        // Process the data in the buffer
+        if (send(newPlayerSocketFd, buffer, 1024, 0) == -1)
+        {
+            printf("Error sending gamestate to new player \n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    fclose(ptr);
 }
 
+int receivedFirstJoin(int newPlayerSocketFd, char *filePath)
+{
+    FILE *ptr;
+    char buffer[1024];
+    ssize_t bytesReceived;
+    ptr = fopen(filePath, "r+");
+    while ((bytesReceived = recv(newPlayerSocketFd, buffer, 1024, 0)) > 0)
+    {
+        if (fwrite(buffer, 1, bytesReceived, ptr) != bytesReceived)
+        {
+            perror("Failed to write data to file");
+            exit(1);
+        }
+    }
+    fclose(ptr);
+}
+
+int requestGameState(int msgid, char *filePath)
+{
+    // Step 1: Send the request to game via msgid
+    //      The request has the form: "?GameState"
+    // Step 2: Wait for the response from game
+    // Step 3: Write into the results
+    //      Step 3.1: if results is NULL => return -1
+    //      Step 3.2: if *results is NULL => allocate *results
+    //      Step 3.3: write the results into results
+
+    // +++ YOUR CODE HERE +++ //
+    // FILE *fptr;
+    msg requestPython = {C_TO_PY, "?GameState"};
+    sprintf(requestPython.msg_text, "?GameState:%s", filePath);
+    msg GameState = {PY_TO_C, NULL};
+    if (msgsnd(msgid, &requestPython, sizeof(requestPython), 0) == -1)
+    {
+        printf("msgsnd ?GameState failed\n");
+    }
+    if (msgrcv(msgid, &GameState, sizeof(GameState), PY_TO_C, 0) == -1)
+    {
+        printf("msgrev GameState Failed\n");
+    }
+    if (strcmp(GameState.msg_text, "Done") == 0)
+    {
+
+        return 0;
+    }
+    return 1;
+}
--- Session/Room/Room.h
+++ Session/Room/Room.h
@@ -11,25 +11,21 @@
 #include <stdio.h>
 
 #include "../Player/Player.h"
-#include "../Client/Client.h"
 
-typedef struct {
-    char* name; // name of room
-    int maxPlayer; // maximun number of players in room
+typedef struct
+{
+    char *name;        // name of room
+    int maxPlayer;     // maximun number of players in room
     int currentNumber; // current number of players in room
-    Player *players; // array of all players in room
+    Player *players;   // array of all players in room
 } Room;
-
-
 
 int isFull(Room room);
 
+Room createRoom(char *roomName, int maxPlayer, Player host);
+int destroyRoom(Room *room);
 
-
-Room createRoom(char* roomName, int maxPlayer, Player host);
-int destroyRoom(Room* room);
-
-int discorverRoom(char*** hostIPs, char*** roomNames);
+int discorverRoom(char ***hostIPs, char ***roomNames);
 int joinRoom(char *hostIPaddr, char *roomName);
 
 int connectToRoomNetwork(Room room, int **clientSocketFds);
@@ -38,6 +34,7 @@ int sendRoomIn4(int newPlayerSocketFd, Room room);
 
 void addPlayer(Room *room, Player *newPlayer);
 
-int sendGameStateToNewPlayer(char *encodedGameState, int newPlayerSocketFd);
+int sendGameStateToNewPlayer(char *filePath, int newPlayerSocketFd);
+int requestGameState(int msgid, char *filePath);
 
 #endif /* Session_h */
